5fec3792e75acfd6e9e489ed4cfca0fb54a052b7
CY GUO <93991166+MOLLY232@users.noreply.github.com>
2022-05-16 21:35:25 +0800

    Create playlist2.js

---
+++ Scripts/playlist2.js
@@ -0,0 +1,42 @@
+trackList = [
+	{
+		name:"Asiaboy-wake up in the moring",
+		artist:"Asiaboy",
+		src:"music/禁藥王-WAKE UP IN THE MORNING.mp3"
+	},
+        {
+		name:"Asiaboy 禁藥王 Lizi 栗子 - 都關掉 Feat. 蛋頭 BG8Locc",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 都關掉 Feat 蛋頭 BG8Locc.mp3"
+        },
+	{
+		name:"Asiaboy - 藥王回來到",
+		artist:"Asiaboy",
+		src:"music/Asiaboy - 藥王回來到.mp3"
+	},
+        {
+		name:"Asiaboy - Everybody Fake Feat. 婁峻碩SHOU",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 Everybody Fake Feat 婁峻碩SHOU .mp3"
+        },
+	{
+		name:"Asiaboy 禁藥王 - 拖鞋 feat. Sowut.mp3",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 - 拖鞋 feat. Sowut.mp3"
+	},
+        {
+		name:"Asiaboy 禁藥王 Lizi 栗子 - PIMP",
+		artist:"Asiaboy",
+		src:"music/Asiaboy 禁藥王 Lizi 栗子 - PIMP.mp3"
+        },
+	{
+		name:"莫宰羊 - 國王 King (Cypher) ft. Asiaboy Lizi Gambler",
+		artist:"Asiaboy",
+		src:"music/莫宰羊 - 國王 King (Cypher) ft. Asiaboy Lizi Gambler .mp3"
+	},
+       	{
+		name:"華興-周湯豪Nickthereal婁峻碩SHOU蛋頭BG8LOCC禁藥王Sowut",
+		artist:"Asiaboy",
+		src:"music/華興-周湯豪Nickthereal婁峻碩SHOU蛋頭BG8LOCC禁藥王Sowut.mp3"
+	}
+];
