50113190ff3a090d9513f58fcc9c5f5d9a187f9b
CY GUO <93991166+MOLLY232@users.noreply.github.com>
2022-05-20 07:51:00 +0800

    Update playlist11.js

--- Scripts/playlist11.js
+++ Scripts/playlist11.js
@@ -2,39 +2,39 @@ trackList = [
 	{
 		name:"棉花糖  有禮的謊", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖  有禮的謊.mp3'
+    src:"MMM/棉花糖/棉花糖  有禮的謊.mp3"
 
 	},
 	{
 		name:"棉花糖  貳拾貳", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖  貳拾貳.mp3'
+    src:"MMM/棉花糖/棉花糖  貳拾貳.mp3"
 
 
 	},
 	{
 		name:"棉花糖 Bu Fu Ci Sheng", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 Bu Fu Ci Sheng.mp3'
+    src:"MMM/棉花糖/棉花糖 Bu Fu Ci Sheng.mp3"
 
 	},
 	{
 		name:"棉花糖 katncandix2  馬戲團公約 Circus Convention 自製MV公視他們在畢業的前一天爆炸 片頭曲", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2  馬戲團公約 Circus Convention 自製MV公視他們在畢業的前一天爆炸 片頭曲.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2  馬戲團公約 Circus Convention 自製MV公視他們在畢業的前一天爆炸 片頭曲.mp3"
 
 	},
 	{
 		name:"棉花糖 katncandix2 一切都是為了與你歌唱明天會好我還是這麼相信", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2 一切都是為了與你歌唱明天會好我還是這麼相信.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2 一切都是為了與你歌唱明天會好我還是這麼相信.mp3"
 
 
 	},
 	{
 		name:"棉花糖 katncandix2 再見王子Official Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2 再見王子Official Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2 再見王子Official Music Video.mp3"
 
 
 	},
@@ -42,69 +42,68 @@ trackList = [
 	{
 		name:"棉花糖 katncandix2 回不去的旅人Official Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2 回不去的旅人Official Music Video.mp3'
-
+    src:"MMM/棉花糖/棉花糖 katncandix2 回不去的旅人Official Music Video.mp3"
 
 	},
 	{
 		name:"棉花糖 katncandix2 好日子Official Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2 好日子Official Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2 好日子Official Music Video.mp3"
 
 
 	},
 	{
 		name:"棉花糖 katncandix2 怎麼說呢Official Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2 怎麼說呢Official Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2 怎麼說呢Official Music Video.mp3"
 
 
 	},
 	{
 		name:"棉花糖 katncandix2一切都是為了與你相遇 Nice To Meet YouOfficial Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2一切都是為了與你相遇 Nice To Meet YouOfficial Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2一切都是為了與你相遇 Nice To Meet YouOfficial Music Video.mp3"
 
 
 	},
 	{
 		name:"棉花糖 katncandix2我們來拍張照 Say CheeseOfficial Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2我們來拍張照 Say CheeseOfficial Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2我們來拍張照 Say CheeseOfficial Music Video.mp3"
 
 
 	},
 	{
 		name:"棉花糖 katncandix2我常常想起你 I Often Think Of YouOfficial Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2我常常想起你 I Often Think Of YouOfficial Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2我常常想起你 I Often Think Of YouOfficial Music Video.mp3"
 
 	},
 	{
 		name:"棉花糖 katncandix2東京下雨了Official Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2東京下雨了Official Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2東京下雨了Official Music Video.mp3"
 
 
 	},
 	{
 		name:"棉花糖 katncandix2親愛的朋友 Dear FriendOfficial Music Video", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 katncandix2親愛的朋友 Dear FriendOfficial Music Video.mp3'
+    src:"MMM/棉花糖/棉花糖 katncandix2親愛的朋友 Dear FriendOfficial Music Video.mp3"
 
 
 	},
 	{
 		name:"棉花糖 Ni Lai Zi Xing Xing", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 Ni Lai Zi Xing Xing.mp3'
+    src:"MMM/棉花糖/棉花糖 Ni Lai Zi Xing Xing.mp3"
 
 	},
 
 	{
 		name:"棉花糖 變成你們喜歡的我", 
     artist:"棉花糖", 
-    src:"MMM/棉花糖/棉花糖 變成你們喜歡的我.mp3'
+    src:"MMM/棉花糖/棉花糖 變成你們喜歡的我.mp3"
 
 	}
 ];
