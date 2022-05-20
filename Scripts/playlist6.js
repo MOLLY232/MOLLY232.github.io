330e748490d4ff054047b6b690c31b5df6ffdf52
CY GUO <93991166+MOLLY232@users.noreply.github.com>
2022-05-20 08:14:16 +0800

    Update playlist6.js

--- Scripts/playlist6.js
+++ Scripts/playlist6.js
@@ -11,17 +11,11 @@ trackList = [
 		src:"PP/yt1s.com - EP164 不敗學習力讀後心得學霸沒告訴你的10個秘密.mp3"
 
 	},
+	
 	{
 		name:"yt1s.com - 日更寫作如何做到敏迪和周欽華聊我們是如何寫作的", 
 		artist:"敏迪選讀",
 		src:"PP/yt1s.com - 日更寫作如何做到敏迪和周欽華聊我們是如何寫作的.mp3"
-
-	},
-	
-	{
-		name:"yt1s.com - 呱吉直播呱吉電台EP116好渣又好帥", 
-		artist:"呱吉", 
-		src:"PP/yt1s.com - 呱吉直播呱吉電台EP116好渣又好帥.mp3"
 
 	}
 ];
