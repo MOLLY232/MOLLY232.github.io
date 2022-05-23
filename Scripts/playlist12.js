cea9a2bdf4c7161c1ca014648e6598a93f96fde9
CY GUO <93991166+MOLLY232@users.noreply.github.com>
2022-05-24 07:33:52 +0800

    Update playlist12.js

--- Scripts/playlist12.js
+++ Scripts/playlist12.js
@@ -1,35 +1,49 @@
 trackList = [
 	{
-	name:"告五人 Accusefive  告五人歌曲合集 2022  Best Songs Of Accusefive 2022  2022 流行 歌曲 告五人.mp3", artist:"告五人", src:"MMM/告五人/告五人 Accusefive  告五人歌曲合集 2022  Best Songs Of Accusefive 2022  2022 流行 歌曲 告五人.mp3"
+	name:"告五人 Accusefive  告五人歌曲合集 2022  Best Songs Of Accusefive 2022  2022 流行 歌曲 告五人.mp3", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 Accusefive  告五人歌曲合集 2022  Best Songs Of Accusefive 2022  2022 流行 歌曲 告五人.mp3"
 
 
 
 	},
 	{
-	name:"告五人 Accusefive  唯一 The One And Only.mp3", artist:"告五人", src:"MMM/告五人/告五人 Accusefive  唯一 The One And Only.mp3"
+	name:"告五人 Accusefive  唯一 The One And Only.mp3", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 Accusefive  唯一 The One And Only.mp3"
 
 
 
 	},
 	{
-	name:"告五人 Accusefive  愛在夏天盛夏之約 Love In Summer  Official Music Video.mp3", artist:"告五人", src:"MMM/告五人/告五人 Accusefive  愛在夏天盛夏之約 Love In Summer  Official Music Video.mp3"
+	name:"告五人 Accusefive  愛在夏天盛夏之約 Love In Summer  Official Music Video.mp3", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 Accusefive  愛在夏天盛夏之約 Love In Summer  Official Music Video.mp3"
 
 	},
 	{
-	name:"告五人 Accusefive  我以為你不會出現 Take Me With You  Official Music VideoUAG安全感形象曲.mp3", artist:"告五人", src:"MMM/告五人/告五人 Accusefive  我以為你不會出現 Take Me With You  Official Music VideoUAG安全感形象曲.mp3"
+	name:"告五人 Accusefive  我以為你不會出現 Take Me With You  Official Music VideoUAG安全感形象曲.mp3", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 Accusefive  我以為你不會出現 Take Me With You  Official Music VideoUAG安全感形象曲.mp3"
 
 	},
 	{
-	name:"告五人 Accusefive 愛人錯過 Somewhere in timeOfficial Music Video (1).mp3", artist:"告五人", src:"MMM/告五人/告五人 Accusefive 愛人錯過 Somewhere in timeOfficial Music Video (1).mp3"
+	name:"告五人 Accusefive 愛人錯過 Somewhere in timeOfficial Music Video (1).mp3", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 Accusefive 愛人錯過 Somewhere in timeOfficial Music Video (1).mp3"
 
 	},
 	{
-	name:"告五人 Accusefive醜人多作怪 Mischief中文歌詞 Lyrics.mp3", artist:"告五人", src:"MMM/告五人/告五人 Accusefive醜人多作怪 Mischief中文歌詞 Lyrics.mp3"
+	name:"告五人 Accusefive醜人多作怪 Mischief中文歌詞 Lyrics.mp3", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 Accusefive醜人多作怪 Mischief中文歌詞 Lyrics.mp3"
 
 	},
   
 	{
-	name:"告五人 好不容易 華燈初上NETFLIX 林心如楊謹華 片尾曲 动态歌词Lyrics.mp3'", artist:"告五人", src:"MMM/告五人/告五人 好不容易 華燈初上NETFLIX 林心如楊謹華 片尾曲 动态歌词Lyrics.mp3'"
+	name:"告五人 好不容易 華燈初上NETFLIX 林心如楊謹華 片尾曲 动态歌词Lyrics.mp3'", 
+		artist:"告五人", 
+		src:"MMM/告五人/告五人 好不容易 華燈初上NETFLIX 林心如楊謹華 片尾曲 动态歌词Lyrics.mp3'"
 
 	}
 ];
