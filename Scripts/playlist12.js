4e9f4e60c6f6091323ff527055fc3991a6a14e02
CY GUO <93991166+MOLLY232@users.noreply.github.com>
2022-05-23 20:42:11 +0800

    Create playlist12.js

---
+++ Scripts/playlist12.js
@@ -0,0 +1,189 @@
+trackList = [
+	{
+		name:"5 Seconds Of Summer  Youngblood Lyrics 5SOS.mp3", artist:"ENG", src:"MMM/5 Seconds Of Summer  Youngblood Lyrics 5SOS.mp3"
+
+
+	},
+	{
+		name:"AJ Mitchell  Like Strangers Do Audio.mp3", artist:"ENG", src:"MMM/AJ Mitchell  Like Strangers Do Audio.mp3"
+
+
+	},
+	{
+		name:"棉花糖 Bu Fu Ci Sheng", 
+    artist:"棉花糖", 
+    src:"MMM/棉花糖/棉花糖 Bu Fu Ci Sheng.mp3"
+
+	},
+	{
+		name:"Alessia Cara  Make It To Christmas.mp3", artist:"ENG", src:"MMM/Alessia Cara  Make It To Christmas.mp3"
+
+	},
+	{
+		name:"Andy Grammer  Love Myself Official Audio.mp3", artist:"ENG", src:"MMM/Andy Grammer  Love Myself Official Audio.mp3"
+
+	},
+	{
+		name:"ANGELE  Démons feat Damso  CLIP OFFICIEL .mp3", artist:"ENG", src:"MMM/ANGELE  Démons feat Damso  CLIP OFFICIEL .mp3"
+
+	},
+
+	{
+		name:"Calvin Harris Sam Smith  Promises Lyrics.mp3", artist:"ENG", src:"MMM/Calvin Harris Sam Smith  Promises Lyrics.mp3"
+
+	},
+	{
+		name:"Chevy  Nalba  Morning Coffee Lyrics  BABEL.mp3", artist:"ENG", src:"MMM/Chevy  Nalba  Morning Coffee Lyrics  BABEL.mp3"
+
+	},
+	{
+		name:"Christian French  I Think Too Much Lyrics.mp3", artist:"ENG", src:"MMM/Christian French  I Think Too Much Lyrics.mp3"
+
+	},
+	{
+		name:"Conor Matthews  Older Acoustic.mp3", artist:"ENG", src:"MMM/Conor Matthews  Older Acoustic.mp3"
+
+	},
+	{
+		name:"David Kushner  Mr Forgettable Official Audio.mp3", artist:"ENG", src:"MMM/David Kushner  Mr Forgettable Official Audio.mp3"
+
+	},
+	{
+		name:"Fall into You  Houses On The Hill  ACOUSTIC GROUP.mp3", artist:"ENG", src:"MMM/Fall into You  Houses On The Hill  ACOUSTIC GROUP.mp3"
+
+
+	},
+	{
+		name:"Halsey  You should be sad.mp3", artist:"ENG", src:"MMM/Halsey  You should be sad.mp3"
+
+	},
+	{
+		name:"HONNE  WHAT WOULD YOU DO Feat Pink Sweat Official Video.mp3", artist:"ENG", src:"MMM/HONNE  WHAT WOULD YOU DO Feat Pink Sweat Official Video.mp3"
+
+
+	},
+	{
+		name:"It Was A Masked Christmas Official Video.mp3", artist:"ENG", src:"MMM/It Was A Masked Christmas Official Video.mp3"
+
+	},
+  {
+		name:"Jake Scott  Off Official Music Video.mp3", artist:"ENG", src:"MMM/Jake Scott  Off Official Music Video.mp3"
+
+	},
+   {
+		name:"joan  so good.mp3", artist:"ENG", src:"MMM/joan  so good.mp3"
+
+	},
+   {
+		name:"John Legend  You Deserve It All Official Video.mp3", artist:"ENG", src:"MMM/John Legend  You Deserve It All Official Video.mp3"
+
+	},
+   {
+		name:"Imagine Dragons  Machine Lyrics .mp3", artist:"ENG", src:"MMM/Imagine Dragons  Machine Lyrics .mp3"
+
+	},
+   {
+		name:"Jonas Blue Why Dont We  Dont Wake Me Up Official Video.mp3", artist:"ENG", src:"MMM/Jonas Blue Why Dont We  Dont Wake Me Up Official Video.mp3"
+
+
+	},
+   {
+		name:"Juice WRLD  Justin Bieber  Wandered To LA Official Audio.mp3", artist:"ENG", src:"MMM/Juice WRLD  Justin Bieber  Wandered To LA Official Audio.mp3"
+
+
+	},
+  {
+		name:"Justin Bieber  Christmas Love.mp3", artist:"ENG", src:"MMM/Justin Bieber  Christmas Love.mp3"
+
+	},
+    {
+		name:"Justin Bieber  Drummer Boy Lyric Video ft Busta Rhymes.mp3", artist:"ENG", src:"MMM/Justin Bieber  Drummer Boy Lyric Video ft Busta Rhymes.mp3"
+
+
+	},
+    {
+		name:"Justin Bieber  Someday At Christmas.mp3", artist:"ENG", src:"MMM/Justin Bieber  Someday At Christmas.mp3"
+
+	},
+    {
+		name:"Khalid Normani  Love Lies Lyrics.mp3", artist:"ENG", src:"MMM/Khalid Normani  Love Lies Lyrics.mp3"
+
+	},
+    {
+		name:"Lukas Graham  Happy For You Official Video With Lyrics.mp3", artist:"ENG", src:"MMM/Lukas Graham  Happy For You Official Video With Lyrics.mp3"
+
+	},
+  
+    {
+		name:"Marshmello  AnneMarie  FRIENDS Lyric Video OFFICIAL FRIENDZONE ANTHEM.mp3", artist:"ENG", src:"MMM/Marshmello  AnneMarie  FRIENDS Lyric Video OFFICIAL FRIENDZONE ANTHEM.mp3"
+
+
+	},
+      {
+		name:"Mokita  colorblind Lyrics.mp3", artist:"ENG", src:"MMM/Mokita  colorblind Lyrics.mp3"
+
+
+	},
+  {
+		name:"Mokita  London Lyrics.mp3", artist:"ENG", src:"MMM/Mokita  London Lyrics.mp3"
+
+
+	},
+ {
+		name:"Oh Wonder  Keep On Dancing Lyric Video.mp3", artist:"ENG", src:"MMM/Oh Wonder  Keep On Dancing Lyric Video.mp3"
+
+	},
+   {
+		name:"OneRepublic  Sunshine Official Lyric Video.mp3", artist:"ENG", src:"MMM/OneRepublic  Sunshine Official Lyric Video.mp3"
+
+
+	},
+   {
+		name:"Plain White Ts  Hey There Delilah Official Video.mp3", artist:"ENG", src:"MMM/Plain White Ts  Hey There Delilah Official Video.mp3"
+
+	},
+    {
+	name:"Rita Ora  LET YOU LOVE ME Lyrics.mp3", artist:"ENG", src:"MMM/Rita Ora  LET YOU LOVE ME Lyrics.mp3"
+
+	},
+    {
+		name:"Sam Fischer  For Now Lyric Video.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp3"
+
+	},
+    {
+		name:"Sandro Cavazza Georgia Ku  Love To Lose.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp4"
+
+	},
+    {
+		name:"Sarah Barrios  Have We Met Before with Eric Nam Lyric Video.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp5"
+
+	},
+{
+		name:"Sasha Alex Sloan  when was it over Lyric Video ft Sam Hunt.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp6"
+
+	},
+     {
+		name:"Selena Gomez Ava Max  Break My Heart.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp7"
+
+	},
+ {
+	name:"The Wanted  Glad You Came.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp8"
+
+	},
+{
+	name:"Valentina Ploy  See you in life Official MV.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp9"
+
+	},
+ {
+	name:"Virginia To Vegas  what are we Lyrics.mp3", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp10"
+
+
+	},
+  
+  
+	{
+	name:"Well be fine English Version.mp3'", artist:"ENG", src:"MMM/Sam Fischer  For Now Lyric Video.mp11"
+
+
+	}
+];
