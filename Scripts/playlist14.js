5f607022a1ee317b252718617ad9fe37a16ef6b4
CY GUO <93991166+MOLLY232@users.noreply.github.com>
2022-05-29 21:49:37 +0800

    Create playlist14.js

---
+++ Scripts/playlist14.js
@@ -0,0 +1,169 @@
+trackList = [
+	{
+	name:"benny blanco Marshmello  Vance Joy  You Official Music Video.mp3", artist:"ENG", src:"MMM/英文歌/benny blanco Marshmello  Vance Joy  You Official Music Video.mp3"
+
+	},
+	{
+	name:"Christian French  I Think Too Much Lyrics (1).mp3", artist:"ENG", src:"MMM/英文歌/Christian French  I Think Too Much Lyrics (1).mp3"
+
+
+	},
+	{
+	name:"David Kushner  Mr Forgettable Official Audio.mp3", artist:"ENG", src:"MMM/英文歌/David Kushner  Mr Forgettable Official Audio.mp3"
+
+
+	},
+	{
+	name:"Gabrielle Aplin  When The Lights Go Out Official Audio.mp3", artist:"ENG", src:"MMM/英文歌/Gabrielle Aplin  When The Lights Go Out Official Audio.mp3"
+
+	},
+	{
+	name:"Greyson Chance  shut up Official Video.mp3", artist:"ENG", src:"MMM/英文歌/Greyson Chance  shut up Official Video.mp3"
+
+	},
+	{
+	name:"Gryffin  OneRepublic  You Were Loved Official Video.mp3", artist:"ENG", src:"MMM/英文歌/Gryffin  OneRepublic  You Were Loved Official Video.mp3"
+
+
+	},
+  	{
+	name:"HRVY  Matoma  Good Vibes Official Audio.mp3", artist:"ENG", src:"MMM/英文歌/HRVY  Matoma  Good Vibes Official Audio.mp3"
+
+
+	},
+  
+  	{
+	name:"Jake Scott  Josie Dunne  CWJBHN Lyric Video.mp3", artist:"ENG", src:"MMM/英文歌/Jake Scott  Josie Dunne  CWJBHN Lyric Video.mp3"
+
+
+	},
+  
+  	{
+	name:"Jake Scott  Off Official Music Video.mp3", artist:"ENG", src:"MMM/英文歌/Jake Scott  Off Official Music Video.mp3"
+
+	},
+  
+  	{
+	name:"Jeremy Zucker  always ill care Lyric Video.mp3", artist:"ENG", src:"MMM/英文歌/Jeremy Zucker  always ill care Lyric Video.mp3"
+
+	},
+  
+  	{
+	name:"John K  happiness Official Music Video.mp3", artist:"ENG", src:"MMM/英文歌/John K  happiness Official Music Video.mp3"
+
+	},
+  
+  	{
+	name:"Jonas Blue Why Dont We  Dont Wake Me Up Official Video.mp3", artist:"ENG", src:"MMM/英文歌/Jonas Blue Why Dont We  Dont Wake Me Up Official Video.mp3"
+
+
+	},
+  
+  	{
+	name:"Gryffin  OneRepublic  You Were Loved Official Video.mp3", artist:"ENG", src:"MMM/英文歌/Gryffin  OneRepublic  You Were Loved Official Video.mp3"
+
+
+	},
+  
+  	{
+	name:"Lauv  Love Like That Official Audio.mp3", artist:"ENG", src:"MMM/英文歌/Lauv  Love Like That Official Audio.mp3"
+
+
+	},
+  
+  	{
+	name:"Lukas Graham  Happy For You Official Video With Lyrics.mp3", artist:"ENG", src:"MMM/英文歌/Lukas Graham  Happy For You Official Video With Lyrics.mp3"
+
+	},
+  
+  	{
+	name:"Marshmello  Demi Lovato  Ok Not To Be Ok.mp3", artist:"ENG", src:"MMM/英文歌/Marshmello  Demi Lovato  Ok Not To Be Ok.mp3"
+
+	},
+  
+  	{
+	name:"Mokita  colorblind Lyrics.mp3", artist:"ENG", src:"MMM/英文歌/Mokita  colorblind Lyrics.mp3"
+
+	},
+  
+    	{
+	name:"Nick Jonas  Selfish ft Jonas Brothers Audio.mp3", artist:"ENG", src:"MMM/英文歌/Nick Jonas  Selfish ft Jonas Brothers Audio.mp3"
+
+
+	},
+  
+    	{
+	name:"NOTD Astrid S  I Dont Know Why Lyrics.mp3", artist:"ENG", src:"MMM/英文歌/NOTD Astrid S  I Dont Know Why Lyrics.mp3"
+
+	},
+  
+    	{
+	name:"NOTD Nina Nesbitt  Cry Dancing.mp3", artist:"ENG", src:"MMM/英文歌/NOTD Nina Nesbitt  Cry Dancing.mp3"
+
+	},
+  
+    	{
+	name:"Oh Wonder  Keep On Dancing Audio.mp3", artist:"ENG", src:"MMM/英文歌/Oh Wonder  Keep On Dancing Audio.mp3"
+
+	},
+  
+    	{
+	name:"R3HAB HRVY  Be Okay Dance Video.mp3", artist:"ENG", src:"MMM/英文歌/R3HAB HRVY  Be Okay Dance Video.mp3"
+
+	},
+     	{
+	name:"R3HAB x Sigala x JP Cooper  Runaway Official Music Video.mp3", artist:"ENG", src:"MMM/英文歌/R3HAB x Sigala x JP Cooper  Runaway Official Music Video.mp3"
+
+	},
+  
+      	{
+	name:"Sam Feldt Sam Fischer  Pick Me Up Lyrics.mp3", artist:"ENG", src:"MMM/英文歌/Sam Feldt Sam Fischer  Pick Me Up Lyrics.mp3"
+
+	},
+  
+      	{
+	name:"Sam Fischer  For Now Lyric Video (1).mp3", artist:"ENG", src:"MMM/英文歌/Sam Fischer  For Now Lyric Video (1).mp3"
+
+	},
+  
+      	{
+	name:"Sandro Cavazza Georgia Ku  Love To Lose.mp3", artist:"ENG", src:"MMM/英文歌/Sandro Cavazza Georgia Ku  Love To Lose.mp3"
+
+	},
+  
+      	{
+	name:"Sarah Barrios  Have We Met Before with Eric Nam Lyric Video (1).mp3", artist:"ENG", src:"MMM/英文歌/Sarah Barrios  Have We Met Before with Eric Nam Lyric Video (1).mp3"
+
+	},
+  
+      	{
+	name:"Sasha Sloan  Lie Lyric Video.mp3", artist:"ENG", src:"MMM/英文歌/Sasha Sloan  Lie Lyric Video.mp3"
+
+	},
+  
+      	{
+	name:"Shawn Mendes  When Youre Gone.mp3", artist:"ENG", src:"MMM/英文歌/Shawn Mendes  When Youre Gone.mp3"
+
+
+	},
+  
+      	{
+	name:"SHY Martin  Still the Same Feat Boy In Space Official Video.mp3", artist:"ENG", src:"MMM/英文歌/SHY Martin  Still the Same Feat Boy In Space Official Video.mp3"
+
+	},
+  
+      	{
+	name:"The Chainsmokers  iPad Official Video.mp3", artist:"ENG", src:"MMM/英文歌/The Chainsmokers  iPad Official Video.mp3"
+
+	},
+  
+      	{
+	name:"Virginia To Vegas  what are we Lyrics.mp3", artist:"ENG", src:"MMM/英文歌/Virginia To Vegas  what are we Lyrics.mp3"
+
+	},
+
+	{
+	name:"Zedd  Jasmine Thompson  Funny Lyric Video.mp3'", artist:"ENG", src:"MMM/英文歌/Zedd  Jasmine Thompson  Funny Lyric Video.mp3'"
+
+	}
+];
